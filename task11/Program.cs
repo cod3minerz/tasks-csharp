c769cb5196054116dc3f4ced661ad5ff073ee66c
Hashpix <kirill.krivonos06@gmail.com>
2025-10-11 10:29:42 +0300

    add tasks 1, 2 again

---
+++ task11/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using MyVector; // Подключаем пространство имён с новым классом MyVector<T>
+
+namespace Task11
+{
+    class Program
+    {
+        static void Main()
+        {
+            MyVector<string> lines = new MyVector<string>();
+
+            try
+            {
+                string[] fileLines = File.ReadAllLines("task11.txt");
+                lines.AddAll(fileLines);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка чтения файла: " + e.Message);
+                return;
+            }
+
+            MyVector<string> ipAddresses = new MyVector<string>();
+
+            for (int i = 0; i < lines.Size(); i++)
+            {
+                string line = lines.Get(i);
+                FindIPAddresses(line, ipAddresses);
+            }
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter("output.txt"))
+                {
+                    for (int i = 0; i < ipAddresses.Size(); i++)
+                    {
+                        writer.WriteLine(ipAddresses.Get(i));
+                    }
+                }
+                Console.WriteLine("Найдено IP-адресов: " + ipAddresses.Size());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка записи в файл: " + e.Message);
+            }
+        }
+
+        static void FindIPAddresses(string line, MyVector<string> result)
+        {
+            int i = 0;
+            while (i < line.Length)
+            {
+                if (i > 0 && IsDigit(line[i - 1]))
+                {
+                    i++;
+                    continue;
+                }
+
+                string ip = TryParseIP(line, i);
+
+                if (ip != null)
+                {
+                    int endPos = i + ip.Length;
+                    if (endPos < line.Length && IsDigit(line[endPos]))
+                    {
+                        i++;
+                        continue;
+                    }
+
+                    result.Add(ip);
+                    i = endPos;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+
+        static string TryParseIP(string line, int start)
+        {
+            int pos = start;
+            int partCount = 0;
+
+            for (int part = 0; part < 4; part++)
+            {
+                if (pos >= line.Length || !IsDigit(line[pos]))
+                    return null;
+
+                string num = "";
+                while (pos < line.Length && IsDigit(line[pos]))
+                {
+                    num += line[pos];
+                    pos++;
+                }
+
+                if (num.Length == 0 || num.Length > 3)
+                    return null;
+
+                if (num.Length > 1 && num[0] == '0')
+                    return null;
+
+                int value = int.Parse(num);
+                if (value > 255)
+                    return null;
+
+                partCount++;
+
+                if (part < 3)
+                {
+                    if (pos >= line.Length || line[pos] != '.')
+                        return null;
+                    pos++;
+                }
+            }
+
+            if (partCount == 4)
+            {
+                return line.Substring(start, pos - start);
+            }
+
+            return null;
+        }
+
+        static bool IsDigit(char c)
+        {
+            return c >= '0' && c <= '9';
+        }
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,131 @@
+﻿struct Complex
+{
+    public double Re;
+    public double Im; 
+    
+    
+    public Complex(double re, double im)
+    {
+        Re = re;
+        Im = im;
+    }
+    
+    public Complex Add(Complex other)
+    {
+        return new Complex(Re + other.Re, Im + other.Im);
+    }
+    
+    public Complex Sub(Complex other)
+    {
+        return new Complex(Re - other.Re, Im - other.Im);
+    }
+    
+    public Complex Mul(Complex other)
+    {
+        return new Complex(Re * other.Re - Im * other.Im,
+                           Re * other.Im + Im * other.Re);
+    }
+    
+    public Complex Div(Complex other)
+    {
+        double denom = other.Re * other.Re + other.Im * other.Im;
+        return new Complex((Re * other.Re + Im * other.Im) / denom,
+                           (Im * other.Re - Re * other.Im) / denom);
+    }
+    
+    public double Abs()
+    {
+        return Math.Sqrt(Re * Re + Im * Im);
+    }
+    
+    public double Arg()
+    {
+        return Math.Atan2(Im, Re);
+    }
+    
+    public override string ToString()
+    {
+        return $"{Re} {(Im >= 0 ? "+" : "-")} {Math.Abs(Im)}i";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Complex num = new Complex(0, 0); 
+        while (true)
+        {
+            Console.WriteLine("\nТекущее число: " + num);
+            Console.WriteLine("Выберите действие:");
+            Console.WriteLine("A - Ввести новое число");
+            Console.WriteLine("S - Сложить");
+            Console.WriteLine("D - Вычесть");
+            Console.WriteLine("M - Умножить");
+            Console.WriteLine("V - Разделить");
+            Console.WriteLine("R - Найти модуль");
+            Console.WriteLine("G - Найти аргумент");
+            Console.WriteLine("Q - Выход");
+            Console.Write("Ваш выбор: ");
+
+            char choice = Char.ToUpper(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+
+            switch (choice)
+            {
+                case 'A':
+                    Console.Write("Введите Re: ");
+                    double re = double.Parse(Console.ReadLine());
+                    Console.Write("Введите Im: ");
+                    double im = double.Parse(Console.ReadLine());
+                    num = new Complex(re, im);
+                    break;
+
+                case 'S':
+                    Complex add = InputComplex();
+                    num = num.Add(add);
+                    break;
+
+                case 'D':
+                    Complex sub = InputComplex();
+                    num = num.Sub(sub);
+                    break;
+
+                case 'M':
+                    Complex mul = InputComplex();
+                    num = num.Mul(mul);
+                    break;
+
+                case 'V':
+                    Complex div = InputComplex();
+                    num = num.Div(div);
+                    break;
+
+                case 'R':
+                    Console.WriteLine("Модуль = " + num.Abs());
+                    break;
+
+                case 'G':
+                    Console.WriteLine("Аргумент (в радианах) = " + num.Arg());
+                    break;
+
+                case 'Q':
+                    Console.WriteLine("Выход...");
+                    return;
+
+                default:
+                    Console.WriteLine("Неизвестная команда!");
+                    break;
+            }
+        }
+    }
+
+    static Complex InputComplex()
+    {
+        Console.Write("Введите Re: ");
+        double re = double.Parse(Console.ReadLine());
+        Console.Write("Введите Im: ");
+        double im = double.Parse(Console.ReadLine());
+        return new Complex(re, im);
+    }
+}
